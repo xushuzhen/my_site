dbb6298433cdb09ba512e96308d847f9c1aa8119
许树真 <shuzhen.xu@zebra-c.com>
2017-06-29 00:07:54 +0800

    创建blog index相关

---
+++ static/js/blog/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by xiaoshu on 2017/6/26.
+ */
+// $(function () {
+//     $('#images').hide();
+//     $(document).click(function () {
+//         $('#images').fadeIn('slow');
+//     })
+// })
+// $('#images').hide();
+$(document).ready(function () {
+    $('#welcome').fadeIn('slow');
+})
