7a387ccd6c58fb4bb657e52081d8cc9a64d3ac29
许树真 <shuzhen.xu@zebra-c.com>
2017-06-29 10:41:07 +0800

    修改blog index内容居中css

--- static/js/blog/index.js
+++ static/js/blog/index.js
@@ -1,13 +1,7 @@
 /**
  * Created by xiaoshu on 2017/6/26.
  */
-// $(function () {
-//     $('#images').hide();
-//     $(document).click(function () {
-//         $('#images').fadeIn('slow');
-//     })
-// })
-// $('#images').hide();
+
 $(document).ready(function () {
     $('#welcome').fadeIn('slow');
 })
