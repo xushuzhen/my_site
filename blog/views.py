33ba328bac798d4d36bab2d3ab677d89b7d248af
许树真 <shuzhen.xu@zebra-c.com>
2017-07-11 13:30:15 +0800

    修改content样式

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from blog.models import Class
-
+from blog.models import Article
 
 # Create your views here.
 
@@ -22,43 +22,69 @@ def load_sidebar():
             'UpdateTime': each_class.UpdateTime,
         }
         class_list.append(class_dir)
-    page_dir = {
+    sidebar_dir = {
         'class_list': class_list,
     }
-    return page_dir
+    return sidebar_dir
+
+
+def load_content():
+    articles = Article.objects.filter(Status=1)
+    article_list = []
+    for each_article in articles:
+        article_dir = {
+            'ArticleID': each_article.ArticleID,
+            'DescriptionSEO': each_article.DescriptionSEO,
+            'KeywordsSEO': each_article.KeywordsSEO,
+            'AuthorSEO': each_article.AuthorSEO,
+            'Title': each_article.Title,
+            'Content': each_article.Content,
+            'Class': each_article.Class,
+            'Lable': each_article.Lable,
+            'PageView': each_article.PageView,
+            'Status': each_article.Status,
+            'CreateTime': each_article.CreateTime,
+            'UpdateTime': each_article.UpdateTime,
+        }
+        article_list.append(article_dir)
+    content_dir = {
+        'article_list': article_list,
+    }
+    return content_dir
 
 
 def blog_main(request):
-    page_dir = load_sidebar()
-    return render(request, 'blog/blog_main.html', page_dir)
+    sidebar_dir = load_sidebar()
+    content_dir = load_content()
+    return render(request, 'blog/blog_main.html', sidebar_dir, content_dir)
 
 
 def blog_about_me(request):
-    page_dir = load_sidebar()
-    page_dir['about_me_active'] = 'active'
-    return render(request, 'blog/blog_about_me.html', page_dir)
+    sidebar_dir = load_sidebar()
+    sidebar_dir['about_me_active'] = 'active'
+    return render(request, 'blog/blog_about_me.html', sidebar_dir)
 
 
 def blog_time_line(request):
-    page_dir = load_sidebar()
-    page_dir['time_line_active'] = 'active'
-    return render(request, 'blog/blog_time_line.html', page_dir)
+    sidebar_dir = load_sidebar()
+    sidebar_dir['time_line_active'] = 'active'
+    return render(request, 'blog/blog_time_line.html', sidebar_dir)
 
 
 def blog_statistics(request):
-    page_dir = load_sidebar()
-    page_dir['statistics_active'] = 'active'
-    return render(request, 'blog/blog_statistics.html', page_dir)
+    sidebar_dir = load_sidebar()
+    sidebar_dir['statistics_active'] = 'active'
+    return render(request, 'blog/blog_statistics.html', sidebar_dir)
 
 
 def blog_class(request, now_class_id):
-    page_dir = load_sidebar()
-    page_dir['class_active'] = 'active'
-    page_dir['now_class_id'] = int(now_class_id)
-    return render(request, 'blog/blog_class.html', page_dir)
+    sidebar_dir = load_sidebar()
+    sidebar_dir['class_active'] = 'active'
+    sidebar_dir['now_class_id'] = int(now_class_id)
+    return render(request, 'blog/blog_class.html', sidebar_dir)
 
 
 def blog_about_my_site(request):
-    page_dir = load_sidebar()
-    page_dir['about_my_site_active'] = 'active'
-    return render(request, 'blog/blog_about_my_site.html', page_dir)
+    sidebar_dir = load_sidebar()
+    sidebar_dir['about_my_site_active'] = 'active'
+    return render(request, 'blog/blog_about_my_site.html', sidebar_dir)
