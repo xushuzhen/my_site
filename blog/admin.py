f3db2c982d17c9ed97c59f508ae7b17da62c950d
许树真 <shuzhen.xu@zebra-c.com>
2017-07-12 21:25:42 +0800

    修改admin能修改的模版

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,49 @@
+# coding:utf-8
 from django.contrib import admin
+from blog.models import Class
+from blog.models import Lable
+from blog.models import Article
+
 
 # Register your models here.
+def status_false(modeladmin, request, queryset):
+    queryset.update(Status=0)
+
+
+def status_true(modeladmin, request, queryset):
+    queryset.update(Status=1)
+
+
+status_true.short_description = "状态置1"
+status_false.short_description = "状态置0"
+
+
+@admin.register(Class)
+class ClassDisplay(admin.ModelAdmin):
+    list_display = ('ClassID', 'ClassName', 'ClassAbstract', 'Status', 'CreateTime', 'UpdateTime')
+    search_fields = ('ClassID', 'ClassName')
+    list_filter = ('Status',)
+    list_per_page = 10
+    actions = [status_true, status_false]
+
+
+@admin.register(Lable)
+class LableDisplay(admin.ModelAdmin):
+    list_display = ('LableID', 'LableName', 'Status', 'CreateTime', 'UpdateTime')
+    search_fields = ('LableID', 'LableName')
+    list_filter = ('Status',)
+    list_per_page = 10
+    actions = [status_true, status_false]
+
+
+@admin.register(Article)
+class ArticleDisplay(admin.ModelAdmin):
+    list_display = (
+        'ArticleID', 'Title', 'PageView', 'Status', 'Class', 'Lable', 'CreateTime', 'UpdateTime')
+    search_fields = ('ArticleID', 'Title')
+    list_filter = ('Class', 'Status')
+    list_per_page = 10
+    actions = [status_true, status_false]
+
+
+admin.site.site_header = "66哒"
--- blog/models.py
+++ blog/models.py
@@ -1,34 +1,35 @@
+# coding:utf-8
 from django.db import models
 
 
 # Create your models here.
 class Class(models.Model):
-    ClassID = models.AutoField(primary_key=True, max_length=11)
-    ClassName = models.CharField(max_length=20)
-    ClassAbstract = models.CharField(max_length=50)
-    Status = models.IntegerField()
-    CreateTime = models.DateTimeField()
-    UpdateTime = models.DateTimeField()
+    ClassID = models.AutoField(u'类ID', primary_key=True, max_length=11)
+    ClassName = models.CharField(u'类名', max_length=20)
+    ClassAbstract = models.CharField(u'类简介', max_length=50)
+    Status = models.IntegerField(u'状态')
+    CreateTime = models.DateTimeField(u'创建时间')
+    UpdateTime = models.DateTimeField(u'更新时间')
 
 
 class Lable(models.Model):
-    LableID = models.AutoField(primary_key=True, max_length=11)
-    LableName = models.CharField(max_length=20)
-    Status = models.IntegerField()
-    CreateTime = models.DateTimeField()
-    UpdateTime = models.DateTimeField()
+    LableID = models.AutoField(u'标签ID', primary_key=True, max_length=11)
+    LableName = models.CharField(u'标签名', max_length=20)
+    Status = models.IntegerField(u'状态')
+    CreateTime = models.DateTimeField(u'创建时间')
+    UpdateTime = models.DateTimeField(u'更新时间')
 
 
 class Article(models.Model):
-    ArticleID = models.AutoField(primary_key=True, max_length=11)
-    DescriptionSEO = models.CharField(max_length=500)
-    KeywordsSEO = models.CharField(max_length=100)
-    AuthorSEO = models.CharField(max_length=20)
-    Title = models.CharField(max_length=100)
-    Content = models.TextField()
-    Class = models.CharField(max_length=20)
-    Lable = models.CharField(max_length=20)
+    ArticleID = models.AutoField(u'文章ID', primary_key=True, max_length=11)
+    DescriptionSEO = models.CharField(u'SEO简介', max_length=500)
+    KeywordsSEO = models.CharField(u'SEO关键字', max_length=100)
+    AuthorSEO = models.CharField(u'SEO作者', max_length=20)
+    Title = models.CharField(u'标题', max_length=100)
+    Content = models.TextField(u'正文')
+    Class = models.CharField(u'类别', max_length=20)
+    Lable = models.CharField(u'标签', max_length=20)
     PageView = models.IntegerField()
-    Status = models.IntegerField()
-    CreateTime = models.DateTimeField()
-    UpdateTime = models.DateTimeField()
+    Status = models.IntegerField(u'状态')
+    CreateTime = models.DateTimeField(u'创建时间')
+    UpdateTime = models.DateTimeField(u'更新时间')
