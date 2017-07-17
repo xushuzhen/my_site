7b88d2aedb4f0e79bf7d0bd51c36dfb9f23e52aa
许树真 <shuzhen.xu@zebra-c.com>
2017-07-17 18:28:21 +0800

    返回顶部加入，成功了

--- static/js/blog/back_top.js
+++ static/js/blog/back_top.js
@@ -1,3 +1,20 @@
-$(function () {
-    $.scrollUp();
+window.onscroll = function () {
+    if (document.documentElement.scrollTop + document.body.scrollTop > 100) {
+        $("#go_top").fadeIn();
+    }
+    else {
+        $("#go_top").fadeOut();
+    }
+}
+
+$("#go_top").onclick(function () {
+    var speed = 200;//滑动的速度
+    $('body,html').animate({scrollTop: 0}, speed);
+    return false;
+});
+
+$("#go_top").click(function () {
+    var speed = 200;//滑动的速度
+    $('body,html').animate({scrollTop: 0}, speed);
+    return false;
 });
