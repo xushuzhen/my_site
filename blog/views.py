0859198f48dcd7b617feffbcb586819caae89280
许树真 <shuzhen.xu@zebra-c.com>
2017-07-18 15:28:26 +0800

    修改文章通知作者部分

--- blog/views.py
+++ blog/views.py
@@ -170,7 +170,6 @@ def blog_mine_redirect(request):
 
 
 def blog_class_redirect(request, class_id):
-    # return HttpResponseRedirect('/blog/class/%s/1/' % class_id)
     return HttpResponseRedirect('/class/%s/1/' % class_id)
 
 
