ab1da9055dfd3252c06d87514abffb8365a0b764
许树真 <shuzhen.xu@zebra-c.com>
2017-07-24 12:45:30 +0800

    增加查询js去掉标点符号

--- static/js/blog/search.js
+++ static/js/blog/search.js
@@ -3,6 +3,7 @@
  */
 $('#search-article-button').on('click', function () {
     var key_words = $('#search-article-input').val();
-    key_words = key_words.replace(/ /g, '').replace(/，/g, '').replace(/,/g, '').replace(/\./g, '').replace(/。/g, '');
+    key_words = key_words.replace(/!|@|#|\$|%|\^|&|\*|\(|\)|_|-|=|\+|！|¥|……|（|）|—|——|【|】|「|」|{|}|\[|]|、|\\|\||`|~|·|～|,|\.|\/|<|>|\?|，|。|／|《|》|？/g,'').replace(/ /g, '');
+    console.log(key_words);
     window.location.href = '/search/' + key_words + '/1/';
 });
