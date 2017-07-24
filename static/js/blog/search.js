8df03ae996f41d9ca360f7555cb66e5a4bdb6091
许树真 <shuzhen.xu@zebra-c.com>
2017-07-24 12:57:57 +0800

    增加查询提示框

--- static/js/blog/search.js
+++ static/js/blog/search.js
@@ -2,8 +2,11 @@
  * Created by xiaoshu on 2017/7/24.
  */
 $('#search-article-button').on('click', function () {
+    layer.msg('正在查询，请稍后...', {
+        time: 20000, //20s后自动关闭
+    });
     var key_words = $('#search-article-input').val();
-    key_words = key_words.replace(/!|@|#|\$|%|\^|&|\*|\(|\)|_|-|=|\+|！|¥|……|（|）|—|——|【|】|「|」|{|}|\[|]|、|\\|\||`|~|·|～|,|\.|\/|<|>|\?|，|。|／|《|》|？/g,'').replace(/ /g, '');
+    key_words = key_words.replace(/!|@|#|\$|%|\^|&|\*|\(|\)|_|-|=|\+|！|¥|……|（|）|—|——|【|】|「|」|{|}|\[|]|、|\\|\||`|~|·|～|,|\.|\/|<|>|\?|，|。|／|《|》|？/g, '').replace(/ /g, '');
     console.log(key_words);
     window.location.href = '/search/' + key_words + '/1/';
 });
