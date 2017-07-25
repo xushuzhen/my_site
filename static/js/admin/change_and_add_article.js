a7e856b8aeba1a2a8440b368b3436264cff29984
许树真 <shuzhen.xu@zebra-c.com>
2017-07-25 17:58:31 +0800

    完善一部分自定义页面

--- static/js/admin/change_and_add_article.js
+++ static/js/admin/change_and_add_article.js
@@ -1,4 +1,54 @@
+$(document).ready(function () {
+    var class_arr = $('#class_input').val().split(',');
+    var label_arr = $('#label_input').val().split(',');
+    var class_boxes = document.getElementsByClassName('class_checkbox');
+    var label_boxes = document.getElementsByClassName('label_checkbox');
+    for (var i = 0; i < class_arr.length; i++) {
+        for (var j = 0; j < class_boxes.length; j++) {
+            if (class_arr[i] === class_boxes[j].value) {
+                class_boxes[j].checked = true;
+                break;
+            }
+        }
+    }
+    for (var i = 0; i < label_arr.length; i++) {
+        for (var j = 0; j < label_boxes.length; j++) {
+            if (label_arr[i] === label_boxes[j].value) {
+                label_boxes[j].checked = true;
+                break;
+            }
+        }
+    }
+
+});
+
+$('#class_select_btn').on('click', function () {
+    var boxes = document.getElementsByClassName('class_checkbox');
+    var class_arr = [];
+    for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].checked) {
+            class_arr.push(boxes[i].value)
+        }
+    }
+    var class_str = class_arr.join(',');
+    $('#class_input').val(class_str)
+});
+
+$('#label_select_btn').on('click', function () {
+    var boxes = document.getElementsByClassName('label_checkbox');
+    var label_arr = [];
+    for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].checked) {
+            label_arr.push(boxes[i].value)
+        }
+    }
+    var label_str = label_arr.join(',');
+    $('#label_input').val(label_str)
+});
+
 $('#save_button').on('click', function () {
+    $('#class_select_btn').click();
+    $('#label_select_btn').click();
     var now_path = window.location.href;
     var ArticleID = -1;
     if (now_path.indexOf('change_article') > 0) {
@@ -10,7 +60,7 @@ $('#save_button').on('click', function () {
     var Title = $('#title_input').val();
     var Content = $('#content_input').html();
     var Class = $('#class_input').val();
-    var Label = $('#lable_input').val();
+    var Label = $('#label_input').val();
     var Status = $('#status_select option:selected').val();
     var TimeLine = $('#timeline_select option:selected').val();
     var TimeLineType = $('#timeline_type_select option:selected').val();
